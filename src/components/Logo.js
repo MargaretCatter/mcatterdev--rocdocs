ecd7db838a288995638e691a268b172737916fae
Margaret Catter <30423669+MargaretCatter@users.noreply.github.com>
2022-02-20 23:28:41 -0800

    fuck js. thats it send tweet.

---
+++ src/components/Logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// Path to the logo file on your project
+import mcatterdevLogo from "mcatterdev--rocketdocs--gatsby/static/logo.png";
+
+const Logo = () => (
+	<img
+		src={mcatterdevLogo}
+		alt="Margaret Catter Development Logo"
+		style={{ width: 180 }}
+	/>
+);
+
+export default Logo;
